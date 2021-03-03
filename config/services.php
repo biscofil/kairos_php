3265522183f748998f11415becf080b6c58d7d5b
Filippo <filippo.bisconcin@gmail.com>
2021-03-03 13:03:35 +0100

    fixed services config

--- config/services.php
+++ config/services.php
@@ -33,11 +33,13 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+        'redirect' => env('APP_URL')
     ],
 
     'facebook' => [
         'client_id' => env('FACEBOOK_CLIENT_ID'),
         'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
+        'redirect' => env('APP_URL')
     ],
 
 ];
