96d0ee33cdd4b5a2819390f067ab46cc7bd1d159
Filippo <filippo.bisconcin@gmail.com>
2021-03-06 18:42:26 +0100

    testcase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,33 @@
 
 namespace Tests;
 
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Testing\TestResponse;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+    use DatabaseTransactions;
+
+    /**
+     * @param int $expectedCode
+     * @param TestResponse $response
+     */
+    public function assertResponseStatusCode(int $expectedCode, TestResponse $response): void
+    {
+
+        if (env('TESTING_DUMP_RESPONSE', false)) {
+            if ($response->getStatusCode() !== $expectedCode) {
+                try {
+                    dump($response->json());
+                } catch (\Exception $e) {
+                    dump($response->content());
+                }
+            }
+        }
+
+        $this->assertEquals($expectedCode, $response->getStatusCode());
+    }
+
 }
