a2045f78befba46c62146699bad500ac9776a398
Filippo <filippo.bisconcin@gmail.com>
2021-04-21 20:46:31 +0200

    AuthenticateWithElectionCreatorJwt fails with null token, AuthenticationException thrown instead of abort

--- app/Http/Middleware/AuthenticateWithElectionCreatorJwt.php
+++ app/Http/Middleware/AuthenticateWithElectionCreatorJwt.php
@@ -6,6 +6,7 @@ namespace App\Http\Middleware;
 
 use App\Models\Election;
 use Closure;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Http\Request;
 
 class AuthenticateWithElectionCreatorJwt
@@ -19,6 +20,7 @@ class AuthenticateWithElectionCreatorJwt
      * @param Request $request
      * @param Closure $next
      * @return mixed
+     * @throws \Illuminate\Auth\AuthenticationException
      */
     public function handle(Request $request, Closure $next)
     {
@@ -30,11 +32,14 @@ class AuthenticateWithElectionCreatorJwt
         }
 
         $tokenStrReceived = $request->bearerToken();
+        if (is_null($tokenStrReceived)) {
+            throw new AuthenticationException();
+        };
 
         // get election creator peer and check token
         $userID = $election->peerServerAuthor->checkJwtTokenAndReturnUserID($tokenStrReceived);
         if (is_null($userID)) {
-            abort(403);
+            throw new AuthenticationException();
         };
 
         // prevent users from specifying it manually
