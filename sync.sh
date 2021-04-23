a6ebc2ad94ae9e0a9bb4de204946513921c721eb
Filippo <filippo.bisconcin@gmail.com>
2021-04-23 14:57:30 +0200

    sync script

---
+++ sync.sh
@@ -0,0 +1,75 @@
+rsync -azP \
+    --exclude '.env' \
+    --exclude 'db.env' \
+    --exclude '.git' \
+    --exclude '.idea' \
+    --exclude 'node_modules/*' \
+    --exclude 'bootstrap/cache/*' \
+    --exclude 'storage/app/public/*' \
+    --exclude 'storage/framework/cache/data/*' \
+    --exclude 'storage/framework/sessions/*' \
+    --exclude 'storage/framework/testing/*' \
+    --exclude 'storage/framework/views/*' \
+    --exclude 'storage/logs/*' \
+    --exclude 'mysql-data' \
+    --exclude '_docker/*' \
+    --exclude 'private_key.pem' \
+    --exclude 'public_key.pem' \
+    --exclude 'resources/assets/*' \
+    --exclude 'resources/js/*' \
+    --exclude 'resources/sass/*' \
+    ./ root@peer20.biscofil.it:/root/helios
+rsync -azP _docker/peer20/ root@peer20.biscofil.it:/root/helios
+
+
+
+rsync -azP \
+    --exclude '.env' \
+    --exclude 'db.env' \
+    --exclude '.git' \
+    --exclude '.idea' \
+    --exclude 'node_modules/*' \
+    --exclude 'bootstrap/cache/*' \
+    --exclude 'storage/app/public/*' \
+    --exclude 'storage/framework/cache/data/*' \
+    --exclude 'storage/framework/sessions/*' \
+    --exclude 'storage/framework/testing/*' \
+    --exclude 'storage/framework/views/*' \
+    --exclude 'storage/logs/*' \
+    --exclude 'mysql-data' \
+    --exclude '_docker/*' \
+    --exclude 'private_key.pem' \
+    --exclude 'public_key.pem' \
+    --exclude 'resources/assets/*' \
+    --exclude 'resources/js/*' \
+    --exclude 'resources/sass/*' \
+    ./ root@peer21.biscofil.it:/root/helios
+rsync -azP _docker/peer21/ root@peer21.biscofil.it:/root/helios
+
+
+rsync -azP \
+    --exclude '.env' \
+    --exclude 'db.env' \
+    --exclude '.git' \
+    --exclude '.idea' \
+    --exclude 'node_modules/*' \
+    --exclude 'bootstrap/cache/*' \
+    --exclude 'storage/app/public/*' \
+    --exclude 'storage/framework/cache/data/*' \
+    --exclude 'storage/framework/sessions/*' \
+    --exclude 'storage/framework/testing/*' \
+    --exclude 'storage/framework/views/*' \
+    --exclude 'storage/logs/*' \
+    --exclude 'mysql-data' \
+    --exclude '_docker/*' \
+    --exclude 'private_key.pem' \
+    --exclude 'public_key.pem' \
+    --exclude 'resources/assets/*' \
+    --exclude 'resources/js/*' \
+    --exclude 'resources/sass/*' \
+    ./ root@peer22.biscofil.it:/root/helios
+rsync -azP _docker/peer22/ root@peer22.biscofil.it:/root/helios
+
+
+
+
