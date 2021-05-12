2f28e3c0c68e14eca4485097d4a4afd776a031ef
Filippo <filippo.bisconcin@gmail.com>
2021-05-12 19:02:46 +0200

    styling

--- resources/js/components/NewTrusteeModal.vue
+++ resources/js/components/NewTrusteeModal.vue
@@ -1,33 +1,35 @@
 <template>
-    <div v-if="election">
-        <h3 class="title">New Trustee
-            <a href="javascript:void(0)" @click="closeModal" class="brackets_around">cancel</a>
-        </h3>
+    <div v-if="election" class="card">
+        <div class="card-header">
+            New Trustee
+            <a href="javascr ipt:void(0)" @click="closeModal" class="brackets_around">cancel</a>
+        </div>
+        <div class="card-body">
+            <p>
+                Adding your own trustee requires a good bit more work to tally the election.
+                <br><br>
+                You will need to have trustees generate keypairs and safeguard their secret key.
+                <br><br>
+                If you are not sure what that means, we strongly recommend clicking Cancel and letting Helios tally the
+                election for you.
+            </p>
 
-        <p>
-            Adding your own trustee requires a good bit more work to tally the election.
-            <br><br>
-            You will need to have trustees generate keypairs and safeguard their secret key.
-            <br><br>
-            If you are not sure what that means, we strongly recommend clicking Cancel and letting Helios tally the
-            election for you.
-        </p>
-
-        <div class="form">
-            <div class="form-group row">
-                <label class="col-sm-12 col-lg-2" for="email"> Email </label>
-                <div class="col-sm-12 col-lg-10">
-                    <input type="email" class="form-control" id="email" size="60" v-model="email"/>
+            <div class="form">
+                <div class="form-group row">
+                    <label class="col-sm-12 col-lg-2" for="email"> Email </label>
+                    <div class="col-sm-12 col-lg-10">
+                        <input type="email" class="form-control" id="email" size="60" v-model="email"/>
+                    </div>
                 </div>
-            </div>
 
-            <div class="form-group row">
-                <label class="col-sm-12 col-lg-2" for="url">Url</label>
-                <div class="col-sm-12 col-lg-10">
-                    <input type="url" class="form-control" id="url" size="60" v-model="url"/>
+                <div class="form-group row">
+                    <label class="col-sm-12 col-lg-2" for="url">Url</label>
+                    <div class="col-sm-12 col-lg-10">
+                        <input type="url" class="form-control" id="url" size="60" v-model="url"/>
+                    </div>
                 </div>
+                <button class="btn btn-success" @click="submit">Add Trustee</button>
             </div>
-            <button class="btn btn-success" @click="submit">Add Trustee</button>
         </div>
     </div>
 </template>
