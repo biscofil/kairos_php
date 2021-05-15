b7693db87caf1b6d402d8a4d9dc36b70b5e61443
Filippo <filippo.bisconcin@gmail.com>
2021-05-15 11:51:58 +0200

    Freeze1IAmFreezingElection

---
+++ app/P2P/Messages/Freeze/Freeze1IAmFreezingElection/Freeze1IAmFreezingElectionRequest.php
@@ -0,0 +1,343 @@
+<?php
+
+
+namespace App\P2P\Messages\Freeze\Freeze1IAmFreezingElection;
+
+
+use App\Exceptions\NotYourElectionException;
+use App\Jobs\RunP2PTask;
+use App\Models\Election;
+use App\Models\PeerServer;
+use App\Models\Trustee;
+use App\Models\User;
+use App\P2P\Messages\Freeze\ThisIsMyThresholdBroadcast\ThisIsMyThresholdBroadcast;
+use App\P2P\Messages\P2PMessageRequest;
+use App\P2P\Tasks\GenerateAndSendShares;
+use App\P2P\Tasks\SendAddMeToYourPeersMessageToUnknownPeers;
+use App\Voting\CryptoSystems\ThresholdBroadcast;
+use Illuminate\Support\Facades\Bus;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
+use phpseclib3\Math\BigInteger;
+
+/**
+ * Describes the first message of the three-phase-commit procedure for election freeze
+ * Class Freeze1IAmFreezingElectionRequest
+ * @package App\P2P\Messages
+ * @property Election $election
+ * @property Trustee[] $trustees
+ * @property null|ThresholdBroadcast $senderThresholdBroadcast
+ * @property null|BigInteger $senderShare
+ */
+class Freeze1IAmFreezingElectionRequest extends P2PMessageRequest
+{
+
+    // TODO if coordinator server acts as peer server too
+    //  then the message should also carry the broadcast + share
+
+    public const TIMEOUT = 10; //seconds
+    public Election $election;
+    public array $trustees;
+    public ?ThresholdBroadcast $senderThresholdBroadcast;
+    public ?BigInteger $senderShare;
+
+    /**
+     * @return string
+     */
+    public static function getRequestMessageName(): string
+    {
+        return 'are_you_ready_for_election_freeze_request';
+    }
+
+    /**
+     * IFrozeMyElection constructor.
+     * @param \App\Models\PeerServer $requestSender
+     * @param \App\Models\PeerServer $requestDestination
+     * @param \App\Models\Election $election
+     * @param \App\Models\Trustee[] $trustees
+     * @param \App\Voting\CryptoSystems\ThresholdBroadcast|null $senderThresholdBroadcast
+     * @param \phpseclib3\Math\BigInteger|null $senderShare
+     * @throws \Exception
+     */
+    public function __construct(PeerServer $requestSender,
+                                PeerServer $requestDestination,
+                                Election $election,
+                                array $trustees,
+                                ?ThresholdBroadcast $senderThresholdBroadcast = null,
+                                ?BigInteger $senderShare = null
+    )
+    {
+        parent::__construct($requestSender, [$requestDestination]);
+        $this->election = $election;
+        $this->trustees = $trustees;
+        $this->senderThresholdBroadcast = $senderThresholdBroadcast;
+        $this->senderShare = $senderShare;
+    }
+
+    // #####################################################################
+
+    /**
+     * @param \App\Models\PeerServer $to
+     * @return array
+     * @throws \Exception
+     */
+    public function serialize(PeerServer $to): array
+    {
+
+        $electionData = $this->election->withoutRelations()->toShareableArray();
+        $trusteeData = $this->trustees; // TODO
+
+        /** @var \App\Models\Trustee $meTrustee */
+        $meTrustee = $this->election->getTrusteeFromPeerServer(PeerServer::me());
+
+        $broadcast = null;
+        $share = null;
+
+        if ($meTrustee) {
+            // broadcast
+            $broadcast = $meTrustee->broadcast;
+            // share
+            $senderTrustee = $this->election->getTrusteeFromPeerServer($to);
+            $share = $senderTrustee->share_sent;
+        }
+
+        return [
+            'election' => $electionData,
+            'trustees' => $trusteeData,
+            //
+            'broadcast' => $broadcast ? $broadcast->toArray() : null,
+            'share' => $share ? $share->toHex() : null,
+        ];
+    }
+
+    /**
+     * @param \App\Models\PeerServer $sender
+     * @param array $messageData
+     * @return self
+     * @throws \Illuminate\Validation\ValidationException
+     * @throws \Exception
+     */
+    public static function unserialize(PeerServer $sender, array $messageData): Freeze1IAmFreezingElectionRequest
+    {
+        // TODO validation
+
+        $data = Validator::make($messageData, [
+            'election' => ['required'],
+            'election.uuid' => ['required', 'uuid', 'exists:elections,uuid'],
+            'election.min_peer_count_t' => ['required', 'int'],
+
+            'trustees' => ['required', 'array'],
+
+            'broadcast' => [
+                'nullable',
+                Rule::requiredIf(function () use ($sender, $messageData) {
+                    return $messageData['election']['min_peer_count_t'] > 0
+                        && in_array($sender->domain, array_column($messageData['trustees'], 'domain'));
+                })
+            ],
+            'share' => [
+                'nullable',
+                Rule::requiredIf(function () use ($sender, $messageData) {
+                    return $messageData['election']['min_peer_count_t'] > 0
+                        && in_array($sender->domain, array_column($messageData['trustees'], 'domain'));
+                })
+            ]
+        ])->validate();
+
+        // election
+        $electionData = $data['election'];
+        $election = Election::findFromUuid($electionData['uuid']);
+        $election->fillFromSharedArray($electionData);
+
+        // trustee data
+        $trusteeData = $data['trustees'];
+
+        // broadcast
+        $broadcast = $data['broadcast'];
+        if ($broadcast) {
+            $thresholdBroadcastClass = $election->cryptosystem->getCryptoSystemClass()::ThresholdBroadcastClass;
+            $broadcast = $thresholdBroadcastClass::fromArray($data['broadcast']); // RSA, ELGAMAL
+        }
+
+        // share
+        $share = $data['share'];
+        if ($share) {
+            $share = new BigInteger($share, 16);
+        }
+
+        return new static(
+            $sender,
+            PeerServer::me(),
+            $election,
+            $trusteeData,
+            $broadcast,
+            $share);
+    }
+
+    // #####################################################################
+
+    /**
+     * @return \App\P2P\Messages\Freeze\Freeze1IAmFreezingElection\Freeze1IAmFreezingElectionResponse
+     * @throws \Exception
+     * @noinspection PhpIncompatibleReturnTypeInspection
+     */
+    public function onRequestReceived(): Freeze1IAmFreezingElectionResponse
+    {
+        Log::debug('Freezing election of another peer');
+
+        if ($this->requestSender->id !== $this->election->peer_server_id) {
+            throw new NotYourElectionException();
+        }
+
+        //
+        $this->election->save();
+        $trustees = self::getOrCreateTrustees($this->election, $this->trustees); // TODO check $trustees
+
+        if ($senderTrustee = $this->election->getTrusteeFromPeerServer($this->requestSender)) {
+            $senderTrustee->broadcast = $this->senderThresholdBroadcast;
+            $senderTrustee->share_received = $this->senderShare;
+            $senderTrustee->save();
+        }
+
+
+//        $this->election->freeze();
+
+        $publicKey = null;
+
+        $jobs = [
+            // send a AddMeToYourPeers message to each unknown peer
+            new RunP2PTask(new SendAddMeToYourPeersMessageToUnknownPeers($this->election)),
+        ];
+
+        $meTrustee = $this->election->getTrusteeFromPeerServer(PeerServer::me(), true);
+
+        Log::debug('Generating keypair');
+        $keyPair = $this->election->cryptosystem->getCryptoSystemClass()->generateKeypair();
+        $meTrustee->private_key = $keyPair->sk;
+        $meTrustee->public_key = $keyPair->pk;
+
+        if ($this->election->min_peer_count_t === 0) { // TODO change
+
+            Log::debug('Freeze1IAmFreezingElection > min_peer_count_t is 0 --> no GenerateAndSendShares');
+            // no slack : l-l theshold
+
+            $publicKey = $keyPair->pk;
+
+        } else {
+
+            Log::debug('Freeze1IAmFreezingElection > min_peer_count_t is not 0 --> GenerateAndSendShares');
+
+            Log::debug('Generating polynomial');
+            $polynomial = $keyPair->sk->getThresholdPolynomial($this->election->min_peer_count_t);
+            $meTrustee->polynomial = $polynomial; // save my polynomial
+
+            Log::debug('Generating broadcast');
+            $broadcast = $meTrustee->polynomial->getBroadcast();
+            $meTrustee->broadcast = $broadcast; // save my broadcast
+
+            // t-l threshold
+            $jobs[] = new RunP2PTask(new GenerateAndSendShares($this->election));
+
+        }
+
+        $meTrustee->save();
+//        $jobs[] = new SendP2PMessage(new Freeze2IAmReadyForFreeze(
+//            PeerServer::me(),
+//            $this->from // send back to coordinator
+//            // TODO
+//        ));
+        // TODO after all AddMeToYourPeers messages are done
+        //  > request only to peers with higher "label" and expect requests from peers with lower "label"
+
+        // execute jobs in sequence
+        Log::debug('Bus chain dispatch in 2 seconds');
+        // wait for 2 seconds to allow everyone to generate its polynomial
+        Bus::chain($jobs)->delay(2)->dispatch();
+
+        Log::debug('replying');
+
+        $broadcastToSendBack = null;
+        $shareToSendBack = null;
+        $freezeReady = false;
+
+        if ($senderTrustee = $this->election->getTrusteeFromPeerServer($this->requestSender)) {
+            // sender (coordinator) is a peer
+
+            $senderIdx = $senderTrustee->getPeerServerIndex();
+            $broadcastToSendBack = $meTrustee->broadcast->toArray();
+            $shareToSendBack = $meTrustee->polynomial->getShare($senderIdx);
+            $senderTrustee->share_sent = $shareToSendBack;
+            $senderTrustee->save();
+
+            if (ThisIsMyThresholdBroadcast::areAllSharesReceived($this->election)) {
+                /**
+                 * Skip the second phase message, reuse the current one
+                 */
+                $freezeReady = true;
+            }
+        }
+
+        return new Freeze1IAmFreezingElectionResponse(
+            PeerServer::me(),
+            $this->requestSender,
+            $this->election,
+            $publicKey,
+            $broadcastToSendBack,
+            $shareToSendBack,
+            $freezeReady
+        );
+    }
+
+    /**
+     * @return string
+     */
+    public static function getResponseClass(): string
+    {
+        return Freeze1IAmFreezingElectionResponse::class;
+    }
+
+    /**
+     * Creates trustees (both users and peer servers) if not existing
+     * @param \App\Models\Election $election
+     * @param array $trustees
+     * @return \App\Models\Trustee[]|array|null[]
+     * @throws \Exception
+     */
+    private static function getOrCreateTrustees(Election $election, array $trustees): array
+    {
+        return array_map(function (array $trusteeData) use ($election) {
+
+            // store
+            $t = new Trustee();
+            $t->fillFromSharedArray($trusteeData);
+            if ($ex = Trustee::findUUID($t->uuid)) {
+                //if exists, do not create it
+                Log::debug("Freeze1IAmFreezingElectionResponse > Trustee with uuid $t->uuid exists, skipping creation");
+                return $ex;
+            }
+
+            $t->election()->associate($election)->save();
+            $t->save();
+            if (!is_null($trusteeData['peer_server'])) {
+                $p = new PeerServer();
+                $p->fillFromSharedArray($trusteeData['peer_server']);
+                if ($knownPeer = PeerServer::withDomain($p->domain)->first()) {
+                    $p = $knownPeer; // use existing if domain is known
+                }
+                $t->peerServer()->associate($p)->save();
+            } elseif (!is_null($trusteeData['user'])) {
+                $u = new User();
+                $u->fillFromSharedArray($trusteeData['user']);
+                $t->user()->associate($u)->save();
+            } else {
+                throw new \Exception("peer_server_id and user_id can't both be null");
+            }
+
+            $t->save();
+            return $t;
+
+        }, $trustees);
+    }
+
+}
---
+++ app/P2P/Messages/Freeze/Freeze1IAmFreezingElection/Freeze1IAmFreezingElectionResponse.php
@@ -0,0 +1,182 @@
+<?php
+
+
+namespace App\P2P\Messages\Freeze\Freeze1IAmFreezingElection;
+
+
+use App\Models\Election;
+use App\Models\PeerServer;
+use App\P2P\Messages\Freeze\Freeze2IAmReadyForFreeze\Freeze2IAmReadyForFreeze;
+use App\P2P\Messages\P2PMessageResponse;
+use App\Voting\CryptoSystems\PublicKey;
+use App\Voting\CryptoSystems\ThresholdBroadcast;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
+use phpseclib3\Math\BigInteger;
+
+/**
+ * Describes the response of the first message of the three-phase-commit procedure for election freeze
+ * Class Freeze1IAmFreezingElectionResponse
+ * @package App\P2P\Messages
+ * @property Election $election
+ * @property \App\Voting\CryptoSystems\PublicKey|null $publicKey
+ * @property \App\Voting\CryptoSystems\ThresholdBroadcast|null $broadcast
+ * @property \phpseclib3\Math\BigInteger|null $share
+ * @property bool $freezeReady
+ */
+class Freeze1IAmFreezingElectionResponse extends P2PMessageResponse
+{
+
+    public Election $election;
+    public ?PublicKey $publicKey;
+    public ?ThresholdBroadcast $broadcast;
+    public ?BigInteger $share;
+    public bool $freezeReady;
+
+    /**
+     * @param \App\Models\PeerServer $requestDestination
+     * @param \App\Models\PeerServer $requestSender
+     * @param \App\Models\Election $election
+     * @param \App\Voting\CryptoSystems\PublicKey|null $publicKey
+     * @param \App\Voting\CryptoSystems\ThresholdBroadcast|null $broadcast
+     * @param \phpseclib3\Math\BigInteger|null $share
+     * @param bool $freezeReady
+     */
+    public function __construct(PeerServer $requestDestination, PeerServer $requestSender,
+                                Election $election,
+                                ?PublicKey $publicKey,
+                                ?ThresholdBroadcast $broadcast,
+                                ?BigInteger $share,
+                                bool $freezeReady)
+    {
+        parent::__construct($requestDestination, $requestSender);
+        $this->election = $election;
+        $this->publicKey = $publicKey;
+        $this->broadcast = $broadcast;
+        $this->share = $share;
+        $this->freezeReady = $freezeReady;
+    }
+
+    // #############################################################
+
+    /**
+     * @return array
+     * @throws \Exception
+     * @noinspection PhpIncompatibleReturnTypeInspection
+     */
+    public function serialize(): array
+    {
+        return [
+            'status' => 'freezing, I will send ready later on',
+            'public_key' => $this->publicKey ? $this->publicKey->toArray() : null,
+            'my_broadcast' => $this->broadcast ? $this->broadcast->toArray() : null,
+            'my_share' => $this->share ? $this->share->toHex() : null,
+            'freeze_ready' => $this->freezeReady
+        ];
+    }
+
+    /**
+     * @param \App\Models\PeerServer $requestDestination
+     * @param array $messageData
+     * @param \App\P2P\Messages\Freeze\Freeze1IAmFreezingElection\Freeze1IAmFreezingElectionRequest $requestMessage
+     * @return self
+     * @throws \Illuminate\Validation\ValidationException
+     * @throws \Exception
+     */
+    public static function unserialize(PeerServer $requestDestination, array $messageData, $requestMessage): Freeze1IAmFreezingElectionResponse
+    {
+        $expectingBroadcastShare = $requestMessage->election->min_peer_count_t > 0 // TODO check
+            && $requestMessage->election->getTrusteeFromPeerServer($requestMessage->requestSender);
+
+        $data = Validator::make($messageData, [
+            'public_key' => ['nullable'], //  TODO required when
+            'my_broadcast' => ['nullable', Rule::requiredIf($expectingBroadcastShare)],
+            'my_share' => ['nullable', Rule::requiredIf($expectingBroadcastShare)],
+            'freeze_ready' => ['required', 'bool']
+        ])->validate();
+
+        // get election from request
+        $election = $requestMessage->election;
+
+        // public key without threshold
+        $publicKey = null;
+        if ($data['public_key']) {
+            /** @var PublicKey $pkClass */
+            $pkClass = $election->cryptosystem->getCryptoSystemClass()::PublicKeyClass;
+            $publicKey = $pkClass::fromArray($data['public_key']);
+        }
+
+        // broadcast
+        $broadcast = null;
+        if ($data['my_broadcast']) {
+            $thresholdBroadcastClass = $election->cryptosystem->getCryptoSystemClass()::ThresholdBroadcastClass;
+            $broadcast = $thresholdBroadcastClass::fromArray($data['my_broadcast']); // RSA, ELGAMAL
+        }
+
+        // share
+        $receivedShare = null;
+        if ($data['my_share']) {
+            $receivedShare = new BigInteger($data['my_share'], 16);
+        }
+
+        $freeze_ready = boolval($data['freeze_ready']);
+
+        return new static(
+            PeerServer::me(),
+            $requestDestination,
+            $election,
+            $publicKey,
+            $broadcast,
+            $receivedShare,
+            $freeze_ready
+        );
+    }
+
+    // #############################################################
+
+    /**
+     * TODO
+     * @param $request
+     * @throws \Exception
+     */
+    public function onResponseReceived($request): void
+    {
+        $meTrustee = $this->election->getTrusteeFromPeerServer(PeerServer::me());
+
+        $trustee = $this->election->getTrusteeFromPeerServer($this->requestDestination);
+        if (!$trustee) {
+            Log::error('Received positive confirmation from peer which is not a trustee for this election.');
+            return;
+        }
+
+        if ($this->election->min_peer_count_t === 0) { // TODO change
+
+            $trustee->setPublicKey($this->publicKey);
+
+        } else {
+
+            if ($meTrustee) { // only if the current server (creator, coordinator) is also peer TODO check
+                // store broadcast and share
+                $trustee->broadcast = $this->broadcast;
+                $trustee->share_received = $this->share;
+                $trustee->freeze_ready = $this->freezeReady;
+
+            }
+            $trustee->save();
+
+        }
+
+        Log::debug('Freeze1IAmFreezingElection > checking if all peers are ready');
+        if (Freeze2IAmReadyForFreeze::areAllPeersReady($this->election)) {
+            // if all are ready
+            Log::debug('Freeze1IAmFreezingElection > all peers are ready. Calling ThisIsMyThresholdBroadcast::onAllPeersReady');
+            Freeze2IAmReadyForFreeze::onAllPeersReady($this->election);
+        }
+
+    }
+
+    // #############################################################
+
+
+}
