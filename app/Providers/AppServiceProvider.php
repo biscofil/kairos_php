d01901ec756ac1d880ca94b96b1bd82cf473f70a
Filippo <filippo.bisconcin@gmail.com>
2021-05-31 16:31:07 +0200

    AppServiceProvider ignores error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Models\PeerServer;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
@@ -33,11 +34,17 @@ class AppServiceProvider extends ServiceProvider
 //            return PeerServer::me();
 //        });
 
+        $me = null;
+        try {
+            $me = PeerServer::me(false);
+        } catch (\Exception $e) {
+            Log::warning('Failed PeerServer::me() in AppServiceProvider');
+        }
         // take the RSA keypair of the current server for JWT auth
-        $me = PeerServer::me(false);
+
         if ($me) {
             config(['app.locale' => $me->locale]);
-            config(['app.timezone' => $me->timezone]);
+//            config(['app.timezone' => $me->timezone]);
             if ($me->jwt_public_key && $me->jwt_secret_key) {
                 config(['jwt.keys.private' => $me->jwt_secret_key->toString()]);
                 config(['jwt.keys.public' => $me->jwt_public_key->toString()]);
