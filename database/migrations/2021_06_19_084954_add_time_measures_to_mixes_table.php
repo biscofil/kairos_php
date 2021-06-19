025e77d890024f707db7ba93ad7b647c727572c6
Filippo <filippo.bisconcin@gmail.com>
2021-06-19 10:57:44 +0200

    Mix has time measures

---
+++ database/migrations/2021_06_19_084954_add_time_measures_to_mixes_table.php
@@ -0,0 +1,36 @@
+<?php /** @noinspection PhpUnused */
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddTimeMeasuresToMixesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('mixes', function (Blueprint $table) {
+            $table->unsignedInteger('mixes_generated_in')->nullable();
+            $table->unsignedInteger('proofs_generated_in')->nullable();
+            $table->unsignedInteger('verified_in')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('mixes', function (Blueprint $table) {
+            $table->dropColumn('verified_in');
+            $table->dropColumn('proofs_generated_in');
+            $table->dropColumn('mixes_generated_in');
+        });
+    }
+}
