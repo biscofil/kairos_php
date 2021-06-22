29cb59939c483a1f746a632a26f430eef67fbd38
Filippo <filippo.bisconcin@gmail.com>
2021-06-22 14:16:56 +0200

    removed custom equals() of DecryptionReEncryptionMix.php

--- app/Voting/AnonymizationMethods/MixNets/DecryptionReEncryption/DecryptionReEncryptionMix.php
+++ app/Voting/AnonymizationMethods/MixNets/DecryptionReEncryption/DecryptionReEncryptionMix.php
@@ -5,7 +5,6 @@ namespace App\Voting\AnonymizationMethods\MixNets\DecryptionReEncryption;
 
 
 use App\Voting\AnonymizationMethods\MixNets\Mix;
-use App\Voting\CryptoSystems\ElGamal\EGCiphertext;
 
 /**
  * Class DecryptionReEncryptionMix
@@ -15,34 +14,5 @@ class DecryptionReEncryptionMix extends Mix
 {
 
     use BelongsToDecryptionReEncryptionMixNode;
-
-
-    /**
-     * TODO CHECK!!!!, only Beta is compared !!!!
-     * @param Mix $b
-     * @return bool
-     * @throws \Exception
-     * @noinspection PhpMissingParentCallCommonInspection
-     */
-    public function equals(Mix $b): bool
-    {
-        for ($i = 0; $i < count($this->ciphertexts); $i++) {
-            if (!$this->ciphertexts[$i]->beta->equals($b->ciphertexts[$i]->beta)) {
-
-//                dump(array_map(function (EGCiphertext $ct) {
-//                    return $ct->pk->parameterSet->extractMessageFromSubgroup($ct->beta)->toHex();
-////                    return $ct->getFingerprint();
-//                }, $this->ciphertexts));
-//
-//                dump(array_map(function (EGCiphertext $ct) {
-//                    return $ct->pk->parameterSet->extractMessageFromSubgroup($ct->beta)->toHex();
-////                    return $ct->getFingerprint();
-//                }, $b->ciphertexts));
-
-                return false;
-            }
-        }
-        return true;
-    }
 
 }
